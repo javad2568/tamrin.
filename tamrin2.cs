22ce90234a80d8162700139f169285a375e79a86
javad2568 <117682959+javad2568@users.noreply.github.com>
2022-11-09 10:24:41 -0800

    Add files via upload

---
+++ tamrin2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x=0,y=0;
+            x = Convert.ToInt32(Console.ReadLine());
+           y = Convert.ToInt32(Console.ReadLine());
+            float Number1 = Math.Abs((x-y));
+Double Number2=Math.Pow(Number1,Math.Abs(y));
+Double Number3=Math.Sqrt(Number2);
+            Console.WriteLine(Number3);
+            Console.ReadKey(); 
+        }
+    }
+}
