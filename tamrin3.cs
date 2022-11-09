46dc26cf0351395bae813e17d2e73af3b9784370
javad2568 <117682959+javad2568@users.noreply.github.com>
2022-11-09 11:08:39 -0800

    Add files via upload

---
+++ tamrin3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Project
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0, c = 0;
+             a=Convert.ToInt32(Console.ReadLine());
+             b=Convert.ToInt32(Console.ReadLine());
+             c=Convert.ToInt32(Console.ReadLine());
+            int Num1 = Convert.ToInt32(a);
+            int Num2 = Convert.ToInt32(b);
+            int Num3 = Convert.ToInt32(c);
+            if (a + b > c && a + c > b && b + c > a)
+            {
+                Mohit(a, b, c);
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+            private static void Mohit(int x, int y, int z) {
+                int w = x + y + z;
+                Console.WriteLine(w);
+            Console.ReadKey();
+            }
+
+    }
+}//class
