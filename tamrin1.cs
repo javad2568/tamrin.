e8218bd185e96579894147bf89dbcda151b6fa4a
javad2568 <117682959+javad2568@users.noreply.github.com>
2022-11-09 08:20:35 -0800

    Add files via upload

---
+++ tamrin1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class test
+    {
+        static void Main(string[] args)
+        {
+            int Number=int.Parse(Console.ReadLine());
+            int Num=Convert.ToInt32(Number);
+            Number=Number+10;
+            Console.WriteLine(Number);
+            Console.ReadKey();
+        }
+    }
+}
